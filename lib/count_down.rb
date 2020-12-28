1e53c3aa48e4427b155782d311c5064428b13cec
Peyton Kellogg <pekellogg@hotmail.com>
2020-12-28 00:08:02 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,11 +1,9 @@
 # Write your code here
 
-# count = 10
-# while count >= 0 do
-#   break if count == 0
-#     puts "Happy New Year!"
-#   end
-#   puts count
-#   count -= 1
-# end
-p "test"
+count = 10
+while count >= 1 do
+  puts count
+  count -= 1
+end
+puts "Happy New Year!"
+# p "test"
